c17a19ff5315c73f112ef7a84b4179245886cbfb
Giovaolmos <127804984+Giovaolmos@users.noreply.github.com>
2024-08-22 14:06:30 -0300

    definimos DB_HOST y PORT

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -7,7 +7,7 @@ dotenvConfig({ path: ".development.env" });
 const config = {
   type: "postgres",
   // host: process.env.DB_HOST,
-  host: "postgresdb",
+  host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
@@ -15,8 +15,8 @@ const config = {
   entities: ["dist/**/*.entity.js"],
   migrations: ["dist/migrations/*{.ts,.js}"],
   autoLoadEntities: true,
-  synchronize: false,
-  dropSchema: false,
+  synchronize: true,
+  dropSchema: true,
   // logging: true,
 };
 
--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,28 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { LoggerMiddleware } from './middlewares/logger.middleware';
-import {  ValidationPipe } from '@nestjs/common';
-import {config as dotenvConfig} from "dotenv";
-import { SwaggerModule } from '@nestjs/swagger';
-import { swaggerConfig } from './config/swaggerConfig';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { LoggerMiddleware } from "./middlewares/logger.middleware";
+import { ValidationPipe } from "@nestjs/common";
+import { config as dotenvConfig } from "dotenv";
+import { SwaggerModule } from "@nestjs/swagger";
+import { swaggerConfig } from "./config/swaggerConfig";
 
+dotenvConfig({ path: ".development.env" });
 
-dotenvConfig({ path: '.development.env' });
-
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api', app, document );
+  SwaggerModule.setup("api", app, document);
 
-  app.use(LoggerMiddleware)
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true
-  }));
+  app.use(LoggerMiddleware);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   await app.listen(PORT);
-  console.log(`Server listening on http://localhost:${PORT}`)
+  console.log(`Server listening on http://localhost:${PORT}`);
 }
 bootstrap();
